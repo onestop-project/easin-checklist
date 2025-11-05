44ad6c16fb544831764f0a8de90924ccee45f68f
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-11-05 15:56:57 +0100

    Use reasin help function to get environment codes

--- src/get_data.R
+++ src/get_data.R
@@ -3,8 +3,9 @@ library(dplyr)
 library(readr)
 
 # Get info for all species ####
-valid_environments <- c("MAR", "FRW", "TER", "OLI")
-info_easin_species <- get_species(environment = valid_environments)
+environments <- reasin::environments()
+valid_environments <- environments$env_code
+info_easin_species <- reasin::get_species(environment = valid_environments)
 # Remove duplicates
 info_easin_species <- info_easin_species %>%
   dplyr::distinct(EASINID, .keep_all = TRUE)
